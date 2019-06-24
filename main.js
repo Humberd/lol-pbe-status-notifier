ed10d394e4b7f63f520a14ccf0b0d00f1fbf0a3b
Humberd <humberd2@gmail.com>
2019-06-24 20:29:35 +0200

    Aded asking for a path

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 const fetch = require('node-fetch');
-const player = require('play-sound')(opts = {player: 'vlc.exe'});
 
 const TICK_INTERVAL = 5000;
 const ALARM_DURATION = 1000 * 60 * 2;
 
+let vlcPlayer = 'vlc.exe';
 let status;
 
 function setStatus(newStatus) {
   if (status === 'offline' && newStatus === 'online') {
     console.log('Status Changed!!!');
-    playSound();
+    playSound(ALARM_DURATION);
   }
 
   status = newStatus;
   console.log(`${new Date().toTimeString()} - ${newStatus}`);
 }
 
-function playSound() {
+function playSound(stopAfter) {
+  const player = require('play-sound')(opts = {player: vlcPlayer});
+
   const playerInstance = player.play('Anthem of Europe.mp3', (err) => {
     if (err) {
-      console.error(err)
+      console.error(err);
     }
   });
 
   setTimeout(() => {
     playerInstance.kill();
-  }, ALARM_DURATION);
+  }, stopAfter);
 }
 
 async function loop() {
@@ -46,4 +48,48 @@ async function main(timeInterval) {
   await loop();
 }
 
-main(TICK_INTERVAL);
+// ********* get vlc location *****************
+
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+function askQuestion() {
+  return new Promise((resolve, reject) => {
+    rl.question('Please select a vlc.exe or other player of your choice: ', path => {
+      path = path.trim();
+
+      rl.close();
+      resolve(path);
+    });
+  });
+}
+
+async function getVlcPath() {
+  try {
+    return await askQuestion();
+  } catch (e) {
+    return await getVlcPath();
+  }
+}
+
+async function initialSequence() {
+  const vlcPath = await getVlcPath();
+  if (vlcPath) {
+    vlcPlayer = vlcPath;
+
+    playSound(5000);
+    console.log('------------------------------------------------------------------------------------');
+    console.log('----- Now the player should be opened to check if it works.  -----------------------');
+    console.log('----- If not please restart the program and provide a valid path again. ------------');
+    console.log('------------------------------------------------------------------------------------');
+  }
+
+  return main(TICK_INTERVAL);
+
+}
+
+initialSequence();
